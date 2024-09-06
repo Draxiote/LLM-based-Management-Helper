8b36ae72aa29bb16918c9db462ee8ac1550f1f91
Draxiote <ameykadam505@gmail.com>
2024-09-06 12:51:32 +0530

    Routing and Functions made

--- LLM_web_app/views.py
+++ LLM_web_app/views.py
@@ -8,3 +8,25 @@ def index(request):
     
     # return HttpResponse('This is Home page')
     
+    
+
+def about(request):
+    return render(request, 'main_screen/about.html')
+    
+def contact(request):
+    return render(request, 'main_screen/contact.html')
+
+def courses(request):
+    return render(request, 'main_screen/courses.html')
+
+def home(request):
+    return render(request, 'main_screen/home.html')
+
+def profile(request):
+    return render(request, 'main_screen/profile.html')
+
+def teachers(request):
+    return render(request, 'main_screen/teachers.html')
+
+def update(request):
+    return render(request, 'main_screen/update.html')
